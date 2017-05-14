f4fe8fd770624dfcaa61db228ba764af54204bfb
Ryan Hardeman <Ryan Hardeman>
2017-05-13 20:41:28 -0400

    javascript updates

--- assets/js/java.js
+++ assets/js/java.js
@@ -3,15 +3,25 @@
 var computerChoice = ["a", "b", "c", "d", "e", "f", "g",  "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
 			"w", "x", "y", "z "];
 
+
+//Declareing vars
 var wins = 0;
 var losses = 0;
 var guesses = 0;
 var guessesLeft = 10;
+var lettersGuessed =[];
 
 
 //computer selects random letter
 var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
-var updateGuessesLeft = funtion() {
+//update guessesLeft
+var updateGuessesLeft = function(){
 	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 }
+
+//update funtion to show letters guessed
+var updateLettersGuessed = function(){
+	document.querySelector("#letters").innerHTML = "Your guesses so far: " + lettersGuessed.join(",");
+};
+
